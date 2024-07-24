abe0f2476df9795493147d0d2b42ce2b58ea8ae7
Aman Singh <singhamanrbj@gmail.com>
2024-07-24 11:10:11 +0530

    print the natural number up to i equal to 7

--- Java Program/src/Main.java
+++ Java Program/src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+
+        int n=sc.nextInt();
+        int i=1;
+
+        for(i=1;i<=n;i++){
+            if(i==7){
+                break;
+            }
+
+            System.out.println(i);
+
+        }
+
+
     }
 }
