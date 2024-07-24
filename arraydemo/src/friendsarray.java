e663b1ae98ccfda907b4b00cb18a444a7b53667b
Aman Singh <singhamanrbj@gmail.com>
2024-07-24 12:03:11 +0530

    to print the friends name in array

---
+++ arraydemo/src/friendsarray.java
@@ -0,0 +1,8 @@
+public class friendsarray {
+    public static void main(String[] args) {
+        String[]studentNames ={"Adarsh","yash","Ajay","Adarsh","Anurag"};
+        for(int i=0;i<studentNames.length;i++){
+            System.out.println(studentNames[i]);
+        }
+    }
+}
