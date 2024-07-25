4c143c617228d089a94cae60af32483d799b9d5f
Aman Singh <singhamanrbj@gmail.com>
2024-07-25 11:24:25 +0530

    time complexity iteration

---
+++ arraydemo/src/TimeComplexityWithDebugging/Java.java
@@ -0,0 +1,10 @@
+package TimeComplexityWithDebugging;
+
+public class Java {
+    public static void main(String[] args) {
+        int i,n=12;
+        for (i = 1; i <= n; i = i * 2) {
+            System.out.println("Hello World");
+        }
+    }
+}
--- arraydemo/src/friendsarray.java
+++ arraydemo/src/friendsarray.java
@@ -1,6 +1,7 @@
 public class friendsarray {
     public static void main(String[] args) {
         String[]studentNames ={"Adarsh","yash","Ajay","Adarsh","Anurag"};
+        studentNames[3]="Prashant";
         for(int i=0;i<studentNames.length;i++){
             System.out.println(studentNames[i]);
         }
