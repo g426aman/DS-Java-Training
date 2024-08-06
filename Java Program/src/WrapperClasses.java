09f918e37bacdfbbac545bf770317755c2f654af
g426aman <amansinghisking13@gmail.com>
2024-08-06 12:10:40 +0530

    Wrappere Class in java

---
+++ Java Program/src/WrapperClasses.java
@@ -0,0 +1,9 @@
+public class WrapperClasses {
+    public static void main(String[] args) {
+        int a=3;
+        Integer b=8;
+
+        String c=String.valueOf(a)+ b.toString();
+        System.out.println(c);
+    }
+}
