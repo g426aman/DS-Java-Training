1887f81a7d90395d49c1a59039d1850b6791cab4
g426aman <amansinghisking13@gmail.com>
2024-08-06 12:46:51 +0530

    Expense Manager

--- ExpenseManager/src/Main.java
+++ ExpenseManager/src/Main.java
@@ -2,77 +2,219 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    //pass the database the url,username, password in string
+    static JTextField deleteField;
+    static String url="jdbc:mysql://localhost:3306/expensedb";
+    static String user="root";
+    static String password="";
+    static Connection con;
+    static JTextField expencetypefield, expenceamoutfield, incomeamoutfield;
     public static void main(String[] args) {
-        //pass the database,url,username,password in string
-        String url = "jdbc:mysql://localhost:3306/expensedb";
-        String user = "root";
-        String password = "";
-        try {
-            Connection conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the database successfully");
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //create frame for expense calculator
+        createExpenseCalculcatorFrame();
+    }
 
-            //Design the java UI for expense Calculator
-            JFrame frame=new JFrame("Aman Expenses");
-            JLabel titleLabel=new JLabel("Expense ");
-            titleLabel.setBounds(210, 20, 80, 50);
-            titleLabel.setForeground(Color.BLUE);
-            frame.add(titleLabel);
+    private static void createExpenseCalculcatorFrame() {
 
+        try
+        {
+            con= DriverManager.getConnection(url,user,password);
 
-            JLabel expensetypeLabel=new JLabel("Enter Expense Type ");
-            expensetypeLabel.setBounds(20, 50, 150, 50);
-            expensetypeLabel.setForeground(Color.RED);
-            frame.add(expensetypeLabel);
+            System.out.println("Connected");
 
-            JTextField expensetype=new JTextField();
-            expensetype.setBounds(170, 60, 100, 30);
-            frame.add(expensetype);
+            //Design the java UI for expence Calculator
+            JFrame frame=new JFrame("Aman Expences ");
 
-            JLabel expenseamountLabel=new JLabel("Enter Expense Amount ");
-            expenseamountLabel.setBounds(20, 80, 170, 50);
-            expenseamountLabel.setForeground(Color.RED);
-            frame.add(expenseamountLabel);
+            JLabel titlelabel=new JLabel("Expences");
+            titlelabel.setBounds(210,10,180,50);
+            titlelabel.setForeground(Color.BLUE);
+            frame.add(titlelabel);
 
-            JTextField expenseamount=new JTextField();
-            expenseamount.setBounds(170, 90, 100, 30);
-            frame.add(expenseamount);
+            JLabel expencetypelabel=new JLabel("Enter Expences Type");
+            expencetypelabel.setBounds(20,50,150,50);
+            expencetypelabel.setForeground(Color.RED);
+            frame.add(expencetypelabel);
 
-            JLabel incomeamountLabel=new JLabel("Enter Income Amount ");
-            incomeamountLabel.setBounds(20, 110, 170, 50);
-            incomeamountLabel.setForeground(Color.RED);
-            frame.add(incomeamountLabel);
+            expencetypefield=new JTextField();
+            expencetypefield.setBounds(170,60,100,30);
+            frame.add(expencetypefield);
 
-            JTextField incomeamount=new JTextField();
-            incomeamount.setBounds(170, 120, 100, 30);
-            frame.add(incomeamount);
+            JLabel expenceamountlabel=new JLabel("Enter Expence amount");
+            expenceamountlabel.setBounds(20,80,170,50);
+            expenceamountlabel.setForeground(Color.RED);
+            frame.add(expenceamountlabel);
 
-            JButton saveButton=new JButton("Save");
-            saveButton.setBounds(210, 250, 80, 30);
-            frame.add(saveButton);
-            //to click the save button
-            saveButton.addActionListener(new ActionListener() {
+            expenceamoutfield=new JTextField();
+            expenceamoutfield.setBounds(170,90,100,30);
+            frame.add(expenceamoutfield);
+
+            JLabel incomeamountlabel=new JLabel("Enter Income amount");
+            incomeamountlabel.setBounds(20,110,170,50);
+            incomeamountlabel.setForeground(Color.RED);
+            frame.add(incomeamountlabel);
+
+            incomeamoutfield=new JTextField();
+            incomeamoutfield.setBounds(170,120,100,30);
+            frame.add(incomeamoutfield);
+
+            JButton savebutton=new JButton("Save");
+            savebutton.setBounds(230,200,80,40);
+            frame.add(savebutton);
+
+            JButton deletebutton=new JButton("Delete");
+            deletebutton.setBounds(150,200,80,40);
+            frame.add(deletebutton);
+
+            deletebutton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    //to insert the data into the table
-                    String insert = "insert into expensetb(incomeamount,expensetype,expenseamount) values(?, ?, ?)";
-                    PreparedStatement ps = null;
-                    try {
-                        ps = conn.prepareStatement(insert);
-                        ps.setInt(1,Integer.parseInt(incomeamount.getText()));
-                        ps.setString(2,expensetype.getText());
-                        ps.setInt(3, Integer.parseInt(expenseamount.getText()));
-                        ps.execute();
-                    } catch (SQLException ex) {
-                        throw new RuntimeException(ex);
+
+                    if (savebutton.isVisible())
+                    {
+
+                        //to hide the insert form
+                        incomeamountlabel.setVisible(false);
+                        incomeamoutfield.setVisible(false);
+                        expenceamountlabel.setVisible(false);
+                        expenceamoutfield.setVisible(false);
+                        expencetypefield.setVisible(false);
+                        expencetypelabel.setVisible(false);
+                        savebutton.setVisible(false);
+                        titlelabel.setText("Enter Id to delete Expense");
+                        deleteField= new JTextField();
+                        deleteField.setBounds(50, 60, 100, 40);
+                        frame.add(deleteField);
                     }
+                    else {
+                        String deleteQuery = "delete from expencetb where id = ?";
+                        try {
+                            PreparedStatement ps = con.prepareStatement(deleteQuery);
+                            ps.setInt(1, Integer.parseInt(deleteField.getText()));
+                            ps.executeUpdate();
+                            JOptionPane.showMessageDialog(null,
+                                    "Record has been deleted");
+                            expencetypelabel.setVisible(true);
+                            expenceamountlabel.setVisible(true);
+                            expencetypefield.setVisible(true);
+                            expenceamoutfield.setVisible(true);
+                            savebutton.setVisible(true);
+                            incomeamountlabel.setVisible(true);
+                            incomeamoutfield.setVisible(true);
+                            deleteField.setVisible(false);
+                            titlelabel.setText("Expense Calculator");
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                    }
+                }
+            });
+
+            JButton viewbutton=new JButton("View");
+            viewbutton.setBounds(320,200,80,40);
+            frame.add(viewbutton);
+
+            viewbutton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (savebutton.isVisible())
+                    {
+
+                        //to hide the insert form
+                        incomeamountlabel.setVisible(false);
+                        incomeamoutfield.setVisible(false);
+                        expenceamountlabel.setVisible(false);
+                        expenceamoutfield.setVisible(false);
+                        expencetypefield.setVisible(false);
+                        expencetypelabel.setVisible(false);
+                        savebutton.setVisible(false);
+                        titlelabel.setText("Enter Id to udpate Expense");
+                        deleteField= new JTextField();
+                        deleteField.setBounds(50, 60, 100, 40);
+                        frame.add(deleteField);
+                    }
+                    else {
+                        String deleteQuery = "Select * from expencetb where id = ?";
+                        try {
+                            PreparedStatement ps = con.prepareStatement(deleteQuery);
+                            ps.setInt(1, Integer.parseInt(deleteField.getText()));
+                            ResultSet rs = ps.executeQuery();
+                            while (rs.next())
+                            {
+                                System.out.print("ID: " + rs.getInt("id"));
+                                System.out.print("Income amount: " + rs.getInt("incomeamount"));
+                                System.out.print("Expense Type: " + rs.getString("expencetype"));
+                                System.out.print("Expense Amount: " + rs.getInt("expenceamount"));
+                                incomeamoutfield.setText(String.valueOf(rs.getInt("incomeamount")));
+                                expenceamoutfield.setText(String.valueOf(rs.getInt("expenceamount")));
+                                expencetypefield.setText(rs.getString("expencetype"));
+
+                                JButton updateButton = new JButton("Update");
+                                updateButton.setBounds(380,200,80,40);
+                                frame.add(updateButton);
+                                updateButton.addActionListener(new ActionListener() {
+                                    @Override
+                                    public void actionPerformed(ActionEvent e) {
+                                        String updateQuery = "update expencetb set incomeamount = ?, expencetype = ?, expenceamount = ? where id = ?";
+                                        try {
+                                            PreparedStatement ps = con.prepareStatement(updateQuery);
+                                            ps.setInt(1, Integer.parseInt(incomeamoutfield.getText()));
+                                            ps.setString(2, expencetypefield.getText());
+                                            ps.setInt(3, Integer.parseInt(expenceamoutfield.getText()));
+                                            ps.setInt(4, 31);
+                                            ps.executeUpdate();
+                                        } catch (SQLException ex) {
+                                            throw new RuntimeException(ex);
+                                        }
+
+                                    }
+                                });
+                            }
+                            JOptionPane.showMessageDialog(null,
+                                    "Record has been deleted");
+                            expencetypelabel.setVisible(true);
+                            expenceamountlabel.setVisible(true);
+                            expencetypefield.setVisible(true);
+                            expenceamoutfield.setVisible(true);
+                            savebutton.setVisible(true);
+                            incomeamountlabel.setVisible(true);
+                            incomeamoutfield.setVisible(true);
+                            deleteField.setVisible(false);
+                            titlelabel.setText("Expense Calculator");
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                    }
+                }
+            });
+
+            JButton clearbutton=new JButton("Clear");
+            clearbutton.setBounds(50,200,80,40);
+            frame.add(clearbutton);
+
+            clearbutton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //to clear the form
+                    clearForm();
+
+                }
+            });
+
+            //to click on the save button
+            savebutton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    //To insert the data into table.
+                    insertExpenseIntoDB();
 
                 }
             });
@@ -80,12 +222,37 @@ public class Main {
             frame.setSize(500,400);
             frame.setLayout(null);
             frame.setVisible(true);
-            //to insert the data into table
-
-
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             throw new RuntimeException(e);
         }
+    }
 
+    private static void insertExpenseIntoDB() {
+        String insert="insert into expencetb(incomeamount,expencetype,expenceamount) values(?,?,?)";
+        PreparedStatement ps= null;
+        try {
+            ps = con.prepareStatement(insert);
+            ps.setInt(1, Integer.parseInt(incomeamoutfield.getText()));
+            ps.setString(2,expencetypefield.getText());
+            ps.setInt(3, Integer.parseInt(expenceamoutfield.getText()));
+            ps.executeUpdate();
+
+            //to clear the form after submit data to mysql
+            clearForm();
+
+            JOptionPane.showMessageDialog(null,
+                    "Record has been added");
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static void clearForm() {
+        expencetypefield.setText("");
+        expenceamoutfield.setText("");
+        incomeamoutfield.setText("");
     }
 }
